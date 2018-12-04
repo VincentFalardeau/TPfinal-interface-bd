bbd24addeec32e33acaacaa011399cc850caacec
vincent <falardeau.vince@gmail.com>
2018-12-04 15:27:11 -0500

    Fichier insertion et sequences

---
+++ Sequences.sql
@@ -0,0 +1,13 @@
+--TP final
+--
+--Vincent Falardeau
+--Emile Menard
+--
+--Sequences
+
+create sequence sqCircuits start with 0;
+create sequence sqClients start with 0;
+create sequence sqReservations start with 0;
+
+--Pas demandé, mais fait par choix
+create sequence sqMonuments start with 0;
