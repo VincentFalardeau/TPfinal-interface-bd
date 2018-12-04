c567915c82bcaf76dfe1c4e3187eaff27bfdb817
vincent <falardeau.vince@gmail.com>
2018-12-04 15:30:11 -0500

    Fichier insertion et sequences

---
+++ Insertions.sql
@@ -0,0 +1,67 @@
+--TP final
+--
+--Vincent Falardeau
+--Emile Menard
+--
+--Insertions
+
+insert into monuments values (
+sqMonuments.nextval(), 'Statue de la liberté', 
+'00-09-1875', 'donné par la France aux USA', 'lien', 100,3);
+
+insert into monuments values (
+sqMonuments.nextval(), 'St-Basil', 
+'12-07-1561', 'Symbole de la Russie', 'lien', 200,4);
+
+insert into monuments values (
+sqMonuments.nextval(), 'Sphinx', 
+'00-00-0001', 'Figure mythologique', 'lien', 300,5);
+
+insert into monuments values (
+sqMonuments.nextval(), 'Pyramide Giza', 
+'00-00-0001', 'On ne sait pas comment elle a ete construite', 'lien', 350,5);
+
+insert into monuments values (
+sqMonuments.nextval(), 'Statue de sirene', 
+'00-00-1913', 'A inspire Walt Disney', 'lien', 50,2);
+
+insert into monuments values (
+sqMonuments.nextval(), 'Palais de Versailles', 
+'00-00-1682', 'Residence de Louix XIV', 'lien', 110,3);
+
+insert into monuments values (
+sqMonuments.nextval(), 'Kinderdijk', 
+'00-00-1997', 'Village hollandais pittoresque', 'lien', 500,4);
+
+insert into monuments values (
+sqMonuments.nextval(), 'Big Ben', 
+'31-05-1859', 'Horloge geante', 'lien', 100,4);
+
+insert into monuments values (
+sqMonuments.nextval(), 'Stonehenge', 
+'00-00-0000', 'Le monument prehistorique le plus populaire', 'lien', 300,5);
+
+insert into monuments values (
+sqMonuments.nextval(), 'College Lionel-Groulx', 
+'00-00-1970', 'Nest pas a la station de metro', 'lien', 50, 2);
+
+insert into monuments values (
+sqMonuments.nextval(), 'Vieux-port', 
+'00-00-1642', 'On y tourne des films qui sont supposes se passer en Europe', 'lien', 100,3);
+
+insert into monuments values (
+sqMonuments.nextval(), 'Stationnement de Lionel-Groulx', 
+'00-00-1980', 'Il faut une vignette', 'lien', 400,1);
+
+insert into monuments values (
+sqMonuments.nextval(), 'Centropolis', 
+'00-00-2012', 'Endroit tendance a Laval', 'lien', 200,2);
+
+insert into monuments values (
+sqMonuments.nextval(), 'Parc du Mont-Tremblant', 
+'00-00-1975', 'Endroit de plein air au Quebec', 'lien', 100,3);
+
+insert into monuments values (
+sqMonuments.nextval(), 'Tour Eiffel', 
+'00-00-1889', 'Se trouve a Paris', 'lien', 200,3);
+
